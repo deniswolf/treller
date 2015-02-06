21d692b0fb181b14aada93863f324a4ae0da9077
Denis Wolf <dear.mr.dw@gmail.com>
2015-02-06 02:37:07 +0100

    add plug, cowboy

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Treller.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:cowboy, :plug]]
   end
 
   # Dependencies can be Hex packages:
@@ -25,6 +25,9 @@ defmodule Treller.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:cowboy, "~> 1.0.0"},
+      {:plug, "~> 0.9.0"}
+    ]
   end
 end
