a222c13ba23a67facc46d6cee32df03ccb95d920
Denis Wolf <dear.mr.dw@gmail.com>
2015-02-06 02:38:44 +0100

    first route: "/"

--- lib/treller.ex
+++ lib/treller.ex
@@ -1,2 +1,10 @@
 defmodule Treller do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    send_resp(conn, 200, "the service is available")
+  end
 end
--- test/treller_test.exs
+++ test/treller_test.exs
@@ -1,7 +1,15 @@
 defmodule TrellerTest do
   use ExUnit.Case
+  use Plug.Test
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @otps Treller.init([])
+
+  test "has root" do
+    bubu = conn :get, "/"
+    conn = Treller.call bubu, @opts
+
+    assert conn.status    == 200
+    assert conn.resp_body == "the service is available"
   end
+
 end
